460d72ba449f3a8d251fa163ee15f005e73da9f8
Juan Jose Alvarez <juan.alvarezo.2002@gmail.com>
2024-10-17 14:09:40 -0500

    commit inicial

---
+++ FlavourSpot/src/main/java/com/example/FlavourSpot/FlavourSpotApplication.java
@@ -0,0 +1,13 @@
+package com.example.FlavourSpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavourSpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlavourSpotApplication.class, args);
+	}
+
+}
---
+++ FlavourSpot/src/test/java/com/example/FlavourSpot/FlavourSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FlavourSpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlavourSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
